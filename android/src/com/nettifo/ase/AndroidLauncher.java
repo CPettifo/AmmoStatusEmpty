1d4d86f4e345e9a17aa4245333f43a07014d4408
cpettifo <craig_petti4@hotmail.com>
2023-05-03 11:29:49 +0200

    Initial Commit

---
+++ android/src/com/nettifo/ase/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nettifo.ase;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nettifo.ase.AmmoStatusEmpty;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new AmmoStatusEmpty(), config);
+	}
+}
