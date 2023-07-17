8bfa109074edd2de649467c61a26e4dce25088a7
CPettifo <craig_petti4@hotmail.com>
2023-07-17 09:01:40 +0200

    add new java files

---
+++ desktop/src/com/mygdx/ammostatusempty/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.ammostatusempty;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mygdx.ammostatusempty.AmmoStatusEmpty;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("ammo-status-empty");
+		new Lwjgl3Application(new AmmoStatusEmpty(), config);
+
+	}
+}
